71b14f25e44796c27742b4ff26cbd39c49757eb2
Arthur Hwang <arthur.hwang@yale.edu>
2020-05-21 09:26:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,6 @@
-# This method takes in a string argument of a sentence, returns same sentence with words
+# This method takes in a string argument of a sentence, 
+# returns same sentence with words reversed
+
+def reverse_each_word(string)
+  sentence_array <- string.split(" ")
+  
