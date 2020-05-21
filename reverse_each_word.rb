aa86df16346c8898d3d469823e499d1032b6bda7
Arthur Hwang <arthur.hwang@yale.edu>
2020-05-21 09:33:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,9 @@
 # returns same sentence with words reversed
 
 def reverse_each_word(string)
-  p sentence_array = string.split(" ")
+  sentence_array = string.split(" ")
   sentence_array.each do |word|
     word = word.reverse
 end
   sentence_array.join(" ")
 end
-
-
-reverse_each_word("Hello there, and how are you?")
