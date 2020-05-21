bb6d9211cbb1454a1f819f540fae69b0dcbaaa07
Arthur Hwang <arthur.hwang@yale.edu>
2020-05-21 09:40:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@
 
 def reverse_each_word(string)
   sentence_array = string.split(" ")
-  sentence_array.collect do |word|
+  reversed_sentence = sentence_array.collect do |word|
     word.reverse
 end
   p reversed_sentence.join(" ")
